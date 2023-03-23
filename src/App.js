0a1cbdb5a46e53ee5493e225c6f0a033d5d065fe
Anerazora <arozarenajesus@gmail.com>
2023-03-23 23:34:12 +0100

    Cambiado CSS y Añador producto

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import './App.css';
 import Producto from './Components/Producto';
 import Productos from './Components/Productos';
-
+import Header from './Components/UI/Header';
+import Footer from './Components/UI/Footer';
+import NuevoProducto from './Components/NuevoProducto/NuevoProducto';
+import {useState} from 'react';
 
 
 function App() {
 
-  const productos = [
-    {
-      nombre: '111111',
-      precio: 45.5,
-      fecha: new Date(2023, 2,5)
-    },
-    {
-      nombre: '22222',
-      precio: 45.5,
-      fecha: new Date(2023, 2,6)
-    },
-    {
-      nombre: '333333',
-      precio: 45.5,
-      fecha: new Date(2023, 2,12)
-    }
-]
+  const [productos, setProductos] = useState(
+    [
+      {
+        id: Math.random().toString(),
+        nombre: 'Ratón óptico 1',
+        precio: 15.50,
+        fecha: new Date(2022, 2, 2)
+      },
+      {
+        id: Math.random().toString(),
+        nombre: 'Ratón óptico 2',
+        precio: 25.50,
+        fecha: new Date(2022, 2, 2)
+      },
+      {
+        id: Math.random().toString(),
+        nombre: 'Ratón óptico 3',
+        precio: 9.50,
+        fecha: new Date(2022, 2, 2)
+      },
+      {
+        id: Math.random().toString(),
+        nombre: 'Ratón óptico 4',
+        precio: 10.5,
+        fecha: new Date(2022, 2, 5)
+      }
+    ]
+  )
+
+  const addProducto = (producto) => {
+    //setProductos(productos.push(producto));
+    setProductos((prevProductos) => {
+      return [producto, ...prevProductos];
+    });
+  }
 
 return (
   <div>
-    <p>Hola Mundo</p>
+    <Header />
+    <NuevoProducto addProducto={addProducto} />
     <Productos productos={productos} />
+    <Footer />
   </div>
 );
 }
---
+++ src/Components/UI/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h2>LISTADO DE PRODUCTOS</h2>
+            <p>Con nombre, imagen, fecha y precio</p>
+        </div>
+    )
+}
+
+export default Header;
